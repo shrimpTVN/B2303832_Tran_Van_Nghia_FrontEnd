6ed1c06811ce54dce868db5a978403a0398ba9ae
shrimp <mrshrimptran@gmail.com>
2026-02-10 08:36:30 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <nav>
+    <a href="/" class="navbar-brand"> Ứng dụng Quản lý danh bạ</a>
+    <div class="mr-auto navbar-nav">
+      <li>
+        <router-link :to="{ name: 'contactbook' }" class="nav-link">
+          <i class="fas fa-address-book"></i>
+        </router-link>
+      </li>
+    </div>
+  </nav>
+</template>
